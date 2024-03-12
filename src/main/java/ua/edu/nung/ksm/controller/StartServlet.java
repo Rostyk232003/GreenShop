c76197fb311a53fadcc8a93ccca6bc6d15f3ef4a
Rostyk232003 <rostislavkopko97@gmail.com>
2024-03-12 13:43:38 +0200

    Add FireBase and rebuilt structure of project

---
+++ src/main/java/ua/edu/nung/ksm/controller/StartServlet.java
@@ -0,0 +1,103 @@
+package ua.edu.nung.ksm.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ua.edu.nung.ksm.model.Firebase;
+import ua.edu.nung.ksm.model.User;
+import ua.edu.nung.ksm.view.IndexView;
+
+import java.io.*;
+import java.util.Properties;
+
+
+@WebServlet(name = "StartServlet", urlPatterns = {"/*"}, loadOnStartup = 1)
+public class StartServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String body = "";
+        String context = "";
+
+        switch (request.getPathInfo()) {
+            case "/contacts":
+                context = "<h2>Our Contacts!</h2>\n";
+                break;
+            case "/login":
+                context = "<h2>Login!</h2>\n";
+                context += IndexView.getInstance().getLoginForm();
+                break;
+            case "/forgotpassword":
+                context = "<h2>Restore Password!</h2>\n";
+                break;
+            default:
+                context = "<h2>Hello World from Servlet!</h2>\n";
+        }
+
+
+        body = IndexView.getInstance().getBody(
+                IndexView.getInstance().getHeader(""),
+                IndexView.getInstance().getFooter(""),
+                context);
+
+        out.println(IndexView.getInstance().getPage("Green Shop", body));
+
+        User user = new User();
+        user.setEmail("emaild@email.com");
+        user.setPassword("112211221122");
+        user.setDisplayName("Test User");
+        Firebase firebase = Firebase.getInstance();
+        if(firebase.getUserByEmail(user.getEmail()) != "OK"){
+            // TODO Rostyslav: login user
+            System.out.println("User Exist");
+        } else {
+            String userMsg = Firebase.getInstance().createUser(user);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String contextPath = request.getContextPath();
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        System.out.println(user);
+
+        response.sendRedirect("/");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        String path = getServletContext().getRealPath("html/");
+        IndexView indexView = IndexView.getInstance();
+        indexView.setPath(path);
+
+        String[] firebaseConfig = readFirebaseConfig();
+        Firebase.getInstance().setFirebaseConfigPath(firebaseConfig[0]);
+        Firebase.getInstance().setFirebaseName(firebaseConfig[1]);
+        Firebase.getInstance().init();
+
+    }
+
+    private String[] readFirebaseConfig() {
+        Properties props = new Properties();
+        String[] firebasrProp = new String[2];
+        InputStream is = getClass().getClassLoader().getResourceAsStream("app.properties");
+        try {
+            props.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        firebasrProp[0] = props.getProperty("file.path");
+        firebasrProp[1] = props.getProperty("firebase.name");
+        return firebasrProp;
+    }
+}
