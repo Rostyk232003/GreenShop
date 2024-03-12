74ed21aff0ef0a96032960785f95019a41a103f0
Rostyk232003 <rostislavkopko97@gmail.com>
2024-03-12 16:54:17 +0200

    Create Hello world with Servlet

--- src/main/java/ua/edu/nung/ksm/model/User.java
+++ src/main/java/ua/edu/nung/ksm/model/User.java
@@ -1,28 +1,19 @@
 package ua.edu.nung.ksm.model;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
-
-import java.util.Objects;
-
 public class User {
     private String email;
     private String password;
-    private String displayName;
     private String phone;
-    private String city;
-    private String street;
+    private String name;
 
-    public User() {}
-
-    public User(String email, String password, String displayName, String phone, String city, String street) {
+    public User(String email, String password, String phone, String name) {
         this.email = email;
         this.password = password;
-        this.displayName = displayName;
         this.phone = phone;
-        this.city = city;
-        this.street = street;
+        this.name = name;
+    }
+
+    public User() {
     }
 
     public String getEmail() {
@@ -49,52 +40,11 @@ public class User {
         this.phone = phone;
     }
 
-    public String getCity() {
-        return city;
+    public String getName() {
+        return name;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "Email='" + email + '\'' +
-                ", Password='" + password + '\'' +
-                ", Phone='" + phone + '\'' +
-                ", City='" + city + '\'' +
-                ", Street='" + street + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPhone(), user.getPhone()) && Objects.equals(getCity(), user.getCity()) && Objects.equals(getStreet(), user.getStreet());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getEmail(), getPassword(), getPhone(), getCity(), getStreet());
-    }
-
 }
