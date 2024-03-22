671d88035c99736510282b7e2db5ac36c028d91b
Rostyk232003 <rostislavkopko97@gmail.com>
2024-03-22 16:11:17 +0200

    add database with users and goods tables. Changes user model and goods model

---
+++ sql.sql
@@ -0,0 +1,25 @@
+create table users
+(
+    id          int auto_increment,
+    email       VARCHAR(40)  not null,
+    password    VARCHAR(120) not null,
+    displayName varchar(20)  null,
+    options     json         null comment 'full user profile',
+    created_at  date         null,
+    deleted_at  date         null,
+    constraint users_pk
+        primary key (id),
+    constraint users_pk_2
+        unique (email)
+);
+
+create table goods
+(
+    id          int auto_increment
+        primary key,
+    name        varchar(60)  not null,
+    description varchar(220) null,
+    brand       varchar(20)  null,
+    photo       json         null,
+    likes       int          null
+);
--- src/main/java/ua/edu/nung/ksm/model/Firebase.java
+++ src/main/java/ua/edu/nung/ksm/model/Firebase.java
@@ -12,6 +12,9 @@ import com.google.gson.JsonParser;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+/**
+ * URL https://firebase.google.com/docs/admin/setup
+ */
 
 public class Firebase {
     public static final String USER_EXISTS = "User exists";
