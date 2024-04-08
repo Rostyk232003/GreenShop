9480271999f81f50806e85a0d6cbc0e85b092150
Rostyk232003 <rostislavkopko97@gmail.com>
2024-04-08 09:31:37 +0300

    Add price

--- src/main/java/ua/edu/nung/ksm/dao/repository/GoodRepository.java
+++ src/main/java/ua/edu/nung/ksm/dao/repository/GoodRepository.java
@@ -1,6 +1,8 @@
 package ua.edu.nung.ksm.dao.repository;
 
 import ua.edu.nung.ksm.dao.entity.Good;
+import ua.edu.nung.ksm.dao.entity.Price;
+import ua.edu.nung.ksm.dao.entity.User;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,7 +14,10 @@ public class GoodRepository {
     public ArrayList<Good> getAll() {
         DataSource dataSource = new DataSource();
         ArrayList<Good> goods = new ArrayList<>();
-        String sql = "Select * FROM goods";
+        String sql = "SELECT g.*, p.* " +
+                "FROM goods g " +
+                "LEFT JOIN prices p ON g.id = p.good_id " +
+                "ORDER BY p.created_at DESC";
 
         try (
                 Connection connection = dataSource.getConnection();
@@ -22,14 +27,23 @@ public class GoodRepository {
         {
             while (resultSet.next()) {
                 Good good = new Good(
-                        resultSet.getLong("id"),
+                        resultSet.getLong(1),
                         resultSet.getString("name"),
                         resultSet.getString("description"),
                         resultSet.getString("brand"),
                         new String[]{resultSet.getString("photo")},
-                        resultSet.getInt("likes")
+                        resultSet.getInt("likes"),
+                        new Price(
+                                resultSet.getLong(7),
+                                resultSet.getLong("good_id"),
+                                resultSet.getDouble("from_supplier"),
+                                resultSet.getDouble("for_client"),
+                                resultSet.getInt("income"),
+                                resultSet.getInt("outcome"),
+                                resultSet.getString("created_at"),
+                                resultSet.getString("deleted_at")
+                        )
                 );
-
                 goods.add(good);
             }
         } catch (SQLException e) {
@@ -39,4 +53,3 @@ public class GoodRepository {
         return goods;
     }
 }
-
