8b487b482cb2e38bdaa7a3bbd6fbe80aded9ad95
Rostyk232003 <rostislavkopko97@gmail.com>
2024-04-23 09:18:36 +0300

    Add likes and logs

--- src/main/java/ua/edu/nung/ksm/controller/GoodsServlet.java
+++ src/main/java/ua/edu/nung/ksm/controller/GoodsServlet.java
@@ -25,7 +25,7 @@ public class GoodsServlet extends HttpServlet {
         String body = goods.stream().map(good -> {
             String goodId = String.valueOf(good.getId());
             return "<div class=\"col-12 col-sm-6 col-lg-4 col-xl-3 my-2\">" +
-                    "<div class=\"card\" style=\"width: 18rem;\">\n" +
+                    "<div class=\"card\">\n" +
                     "<div id=\"carouselExample" + goodId + "\" class=\"carousel slide\">" +
                     "<div class=\"carousel-inner\">"+
                     "<div class=\"carousel-item active\">"+
@@ -45,7 +45,11 @@ public class GoodsServlet extends HttpServlet {
                     "</button>"+
                     "</div>"+
                     "  <div class=\"card-body\">\n" +
-                    "    <h5 class=\"card-title\">" + good.getName() + "</h5>\n" +
+                    "    <h5 class=\"card-title position-relative me-4\">" + good.getName() +
+                    "<span class=\"position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success\">" +
+                    good.getLikes() +
+                    "</span>" +
+                    "</h5>\n" +
                     "    <h6 class=\"card-subtitle mb-2 text-body-secondary\">Price:" + good.getPrice().getFor_client() + " UAH</h6>\n" +
                     "    <p class=\"card-text\">" + good.getShortDescription() + "</p>\n" +
                     "    <a href=\"#\" class=\"card-link\">Card link</a>\n" +
